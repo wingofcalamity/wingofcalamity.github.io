88a63e2525c319f9c43b0d4e406f99648336eecf
wingofcalamity <87781829+wingofcalamity@users.noreply.github.com>
2021-11-30 18:50:55 +0100

    cool stuff part 138

--- pages/script.js
+++ pages/script.js
@@ -17,14 +17,14 @@ function loadDoc(value) {
               xhttp.open("GET", "normal.txt", true);
       }
       xhttp.send(); 
+}
+function dictionary() {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {dict(this)};
+    xhr.open("GET", "dictionary.xml", true);
+    xhr.send();
   }
-  function dictionary() {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function() {dict(this)};
-      xhr.open("GET", "dictionary.xml", true);
-      xhr.send();
-  }
-  function dict(xml) {
+function dict(xml) {
       const xmlDoc = xml.responseXML;
       const x = xmlDoc.getElementsByTagName("CONJ");
       let table=`<tr><th>Plain</th><th>Negative</th><th>Past</th><th>て-Form</th></tr>`;
@@ -40,20 +40,4 @@ function loadDoc(value) {
       `</td></tr>`;
       }
       document.getElementById("dicto").innerHTML = table;
-  }
-
-function dictjson1() {
-    const dbParam = JSON.stringify({table:"customers",limit:20});
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function() {
-      myObj = JSON.parse(this.responseText);
-      let text = "<table border='1'>"
-      for (let x in myObj) {
-        text += "<tr><td>" + myObj[x].name + "</td></tr>";
-      }
-      text += "</table>"
-      document.getElementById("jtable").innerHTML = text;
-    }
-    xmlhttp.open("POST", "dictionary.json");
-    xmlhttp.send("x=" + dbParam);
 }
